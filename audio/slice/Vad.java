d25a9324ab821c37ab72442ea5893f81b232cea8
zhangxiang <zhangxiang@npnets.com>
2023-12-29 10:33:29 +0800

    nothing

--- audio/slice/Vad.java
+++ audio/slice/Vad.java
@@ -1,12 +1,13 @@
 package audio.slice;
 
-import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import org.checkerframework.checker.units.qual.K;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 /**
  * x.z
@@ -14,13 +15,42 @@ import java.util.stream.Collectors;
  */
 public class Vad {
 
-
-
     public static void main(String[] args) throws IOException {
-        HashMap<Integer, ArrayList<String>> listHashMap = new HashMap<>();
-        listHashMap.put(1, Lists.newArrayList("1.mp3"));
-        listHashMap.put(2, Lists.newArrayList("2.mp3","x"));
-        listHashMap.put(3, Lists.newArrayList("3.mp3","y"));
-        System.out.println(listHashMap.values().stream().flatMap(List::stream).collect(Collectors.toList()));
+        Path path = Paths.get("D:\\linuxupload\\hot-rule.txt");
+        List<String> list = Files.readAllLines(path);
+        HashMap<String, String> hotRuleMap = new HashMap<>();
+        list.forEach(e -> {
+            String[] split = e.split("=");
+            hotRuleMap.put(split[0], split[1]);
+        });
+
+
+        Collection<String> values = hotRuleMap.values();
+        HashSet<String> strings = new HashSet<>(values);
+
+        ArrayList<String> strList = new ArrayList<>();
+        for (String s : strings) {
+            if (hotRuleMap.keySet().contains(s)){
+               strList.add(s);
+            }
+        }
+
+        // key value 有重复
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        HashSet<Object> objects = new HashSet<>();
+        hotRuleMap.forEach((k, v) -> {
+            if (strList.contains(v)){
+                hashMap.put(k,v);
+            }
+            if (k.length() == 1){
+                objects.add(k);
+            }
+        });
+        System.out.println(new Gson().toJson(hashMap));
+        System.out.println(objects);
+
+
+
     }
 }
